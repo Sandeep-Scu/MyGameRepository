465ec99c8902e7b4b6c175319870215253675f7b
skaur <skaur@10.0.0.37>
2021-06-06 14:59:48 +0800

    Third Version

---
+++ src/test/java/ass3/mygame2/GameTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ass3.mygame2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author skaur
+ */
+public class GameTest {
+    
+    public GameTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getCurrentRoom method, of class Game.
+     */
+    @Test
+    public void testGetCurrentRoom() {
+      
+    }
+
+   
+    /**
+     * Test of processCommand method, of class Game.
+     */
+    @Test
+    public void testProcessCommand() {
+       
+    }
+    
+}
---
+++ src/test/java/ass3/mygame2/RoomTest.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ass3.mygame2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ *
+ * @author skaur
+ */
+public class RoomTest {
+    
+    public RoomTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of setExit method, of class Room.
+     */
+    @Test
+    public void testSetExit() {
+       
+    }
+
+    /**
+     * Test of getShortDescription method, of class Room.
+     */
+    @Test
+    public void testGetShortDescription() {
+       
+    }
+
+    /**
+     * Test of getLongDescription method, of class Room.
+     */
+    @Test
+    public void testGetLongDescription() {
+        
+    }
+
+    /**
+     * Test of getAllItems method, of class Room.
+     */
+    @Test
+    public void testGetAllItems() {
+       
+    }
+
+   
+    
+}
